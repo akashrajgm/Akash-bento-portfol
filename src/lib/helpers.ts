2f5af1bd9bb925962f2a1a6f5c828e779e965e77
akashrajgm <musicloverakashraj@gmail.com>
2025-02-20 23:16:27 +0530

    Update helpers.ts

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,38 +1,25 @@
 ---
 import { getCurrentTimeInIST, formatTimeForIST } from "../lib/helpers";
 import Card from "./Card/index.astro";
----
 
-<script>
-  import { onCleanup, onMount } from "solid-js";
+let timeDisplay;
 
-  let interval;
-
-  function updateClock() {
-    const timeDisplay = document.getElementById("timeDisplay");
-    const now = getCurrentTimeInIST(); // Use correct function
-
-    if (timeDisplay) {
-      timeDisplay.textContent = formatTimeForIST(now);
-      timeDisplay.setAttribute("datetime", now.toISOString());
-    }
+function updateClock() {
+  const now = getCurrentTimeInIST();
+  if (timeDisplay) {
+    timeDisplay.textContent = formatTimeForIST(now);
+    timeDisplay.setAttribute("datetime", now.toISOString());
   }
+}
 
-  onMount(() => {
-    interval = setInterval(updateClock, 1000);
-  });
-
-  onCleanup(() => {
-    clearInterval(interval);
-  });
-</script>
+setInterval(updateClock, 1000);
+---
 
 <Card colSpan="lg:col-span-2" rowSpan="md:row-span-2" title="Time Zone">
   <time
-    datetime=""
     id="timeDisplay"
     class="text-2xl xl:text-5xl xl:whitespace-nowrap w-50 xl:w-100 h-[calc(100%-28px)] font-serif flex justify-center items-center"
   >
-    {formatTimeForIST(getCurrentTimeInIST())} <!-- Use correct function names -->
+    {formatTimeForIST(getCurrentTimeInIST())}
   </time>
 </Card>
